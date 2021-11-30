cb964b5ef24043986b9f42a61d210320c8cb7a7e
Connor Dieck <connordieck@gmail.com>
2021-11-30 16:51:06 -0500

    finished parts 3 and 4

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -26,3 +26,48 @@ it("works when you click on the right arrow", function() {
 	expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
 	expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
 });
+
+it("works when you click on the left arrow", function() {
+	const { queryByTestId, queryByAltText } = render(<Carousel />);
+
+	// expect the first image to show, but not the second
+	expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+	expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+
+	// move forward in the carousel
+	const rightArrow = queryByTestId("right-arrow");
+	fireEvent.click(rightArrow);
+
+	// expect the second image to show, but not the first
+	expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
+	expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
+
+	// move backward in the carousel
+	const leftArrow = queryByTestId("left-arrow");
+	fireEvent.click(leftArrow);
+
+	// expect the first image to show, but not the second
+	expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+	expect(queryByAltText("Photo by Josh Post on Unsplash")).not.toBeInTheDocument();
+});
+
+it("hides the left arrow when on the first image", function() {
+	const { queryByTestId, queryByAltText } = render(<Carousel />);
+
+	// expect leftArrow to not be in document
+	expect(queryByTestId("left-arrow")).not.toBeInTheDocument();
+});
+
+it("hides the right arrow when on the last image", function() {
+	const { queryByTestId, queryByAltText } = render(<Carousel />);
+
+	// move forward in the carousel
+	const rightArrow = queryByTestId("right-arrow");
+	fireEvent.click(rightArrow);
+
+	// move forward in the carousel
+	fireEvent.click(rightArrow);
+
+	// expect rightArrow to not be in document
+	expect(queryByTestId("right-arrow")).not.toBeInTheDocument();
+});
